78519ed7e73d75d9eb848cddb849d9e030379852
Umang-11 <umangkumarchaurasia@gmail.com>
2023-10-15 17:25:11 +0530

    first commit

---
+++ keys.js
@@ -0,0 +1,7 @@
+//const mongoUrl = "mongodb+srv://umangkumarchaurasia:bdheGFFZIpk8FSD3@cluster0.veuivvo.mongodb.net/?retryWrites=true&w=majority"
+
+module.exports = {
+    mongoUrl : "mongodb+srv://umangkumarchaurasia:bdheGFFZIpk8FSD3@cluster0.veuivvo.mongodb.net/?retryWrites=true&w=majority",
+    Jwt_secret : "qwertyytrewq"
+
+};
